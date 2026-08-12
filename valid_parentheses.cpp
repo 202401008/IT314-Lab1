52ac7562dc4345f86452828d6937e65fa9e06e2f
202401008 <202401008@daiict.ac.in>
2026-08-12 21:58:42 +0530

    Add files via upload

---
+++ valid_parentheses.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <stack>
+#include <string>
+using namespace std;
+
+bool isValid(const string& s) {
+    stack<char> st;
+    for (char c : s) {
+        if (c == '(' || c == '{' || c == '[') {
+            st.push(c);
+        } else {
+            if (st.empty())
+                return false;
+            char top = st.top();
+            if ((c == ')' && top != '(') ||
+                (c == '}' && top != '{') ||
+                (c == ']' && top != '['))
+                return false;
+            st.pop();
+        }
+    }
+    return st.empty();
+}
+
+int main() {
+    string s;
+    cout << "Enter a string of brackets: ";
+    cin >> s;
+    if (isValid(s))
+        cout << "Valid Parentheses" << endl;
+    else
+        cout << "Invalid Parentheses" << endl;
+    return 0;
+}
